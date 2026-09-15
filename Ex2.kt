52cabc88df60d0ede71c24d1ef640e5cd4eccfb5
Aymen El yaakoubi <aymaneyaakoubi528@gmail.com>
2026-09-15 23:20:58 +0200

    Add files via upload

---
+++ Ex2.kt
@@ -0,0 +1,12 @@
+package TP1
+
+fun main () {
+    var nom:String = "Aymen"
+    var age:Int = 19
+    println("j'appel $nom ,et j'ai $age ans")
+    var ville:String = "Rabat"
+    println("j'habit a $ville")
+    var taille:Double = 1.75
+    println("mon taille est $taille")
+    println("Je m'appelle Ahmed, j'ai 20 ans, j'habite à Rabat et je mesure 1.75 m.")
+}
